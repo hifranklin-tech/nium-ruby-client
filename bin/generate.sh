7cc32a6393bc68d41f2e760554d515678196608d
Johan <johanfrolich@gmail.com>
2024-02-28 16:03:46 +0100

    Add script for re-generating client

---
+++ bin/generate.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+openapi-generator generate \
+  --generator-name ruby \
+  --library=faraday \
+  --input-spec ./vendor/nium-openapi/nium.yaml \
+  --output . \
+  --package-name=nium \
+  --additional-properties="moduleName=NiumClient"
